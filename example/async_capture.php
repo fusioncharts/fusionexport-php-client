96ab2ba74ec3f03df2dfd2b1c19370252d93f24a
Jimut Dhali <jemdhali67@gmail.com>
2017-11-27 12:34:13 +0530

    Added async capture example

---
+++ example/async_capture.php
@@ -0,0 +1,31 @@
+<?php
+
+// Exporting a chart
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use FusionExport\ExportManager;
+use FusionExport\ExportConfig;
+
+$exportConfig = new ExportConfig();
+$exportConfig->set('chartConfig', file_get_contents('single.json'));
+$exportConfig->set('callbackFilePath', realpath('expand_scroll.js'));
+$exportConfig->set('asyncCapture', 'true');
+
+$onStateChange = function ($state) {
+  echo('STATE: [' . $state->reporter . '] ' . $state->customMsg . "\n");
+};
+
+$onDone = function ($export, $e) {
+    if ($e) {
+        echo('ERROR: ' . $e->getMessage());
+    } else {
+        foreach ($export as $file) {
+            echo('DONE: ' . $file->realName . "\n");
+            copy($file->tmpPath, $file->realName);
+        }
+    }
+};
+
+$exportManager = new ExportManager();
+$exportManager->export($exportConfig, $onDone, $onStateChange);
