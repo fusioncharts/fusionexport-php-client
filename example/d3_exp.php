5f8a0d000b8481611d14b0ff533ac2b7936679ad
Jimut Dhali <jemdhali67@gmail.com>
2018-01-02 11:38:39 +0530

    Added d3 example

---
+++ example/d3_exp.php
@@ -0,0 +1,37 @@
+<?php
+
+// D3 export
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Use the sdk
+use FusionExport\ExportManager;
+use FusionExport\ExportConfig;
+
+// Instantiate the ExportConfig class and add the required configurations
+$exportConfig = new ExportConfig();
+$exportConfig->set('templateFilePath', realpath('resources/template_d3.html'));
+$exportConfig->set('type', 'pdf');
+$exportConfig->set('asyncCapture', 'true');
+
+// Called on each export state change
+$onStateChange = function ($state) {
+  echo('STATE: [' . $state->reporter . '] ' . $state->customMsg . "\n");
+};
+
+// Called when export is done
+$onDone = function ($export, $e) {
+    if ($e) {
+        echo('ERROR: ' . $e->getMessage());
+    } else {
+        foreach ($export as $file) {
+            echo('DONE: ' . $file->realName . "\n");
+            copy($file->tmpPath, $file->realName);
+        }
+    }
+};
+
+// Instantiate the ExportManager class
+$exportManager = new ExportManager();
+// Call the export() method with the export config and the respective callbacks
+$exportManager->export($exportConfig, $onDone, $onStateChange);
