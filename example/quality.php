a7e5a34765a6ba3b967174e46615b2dcf83a89e0
Jimut Dhali <jemdhali67@gmail.com>
2018-02-26 13:01:34 +0530

    added quality example

---
+++ example/quality.php
@@ -0,0 +1,39 @@
+<?php
+
+// Exporting a chart with best quality
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Use the sdk
+use FusionExport\ExportManager;
+use FusionExport\ExportConfig;
+
+// Instantiate the ExportConfig class and add the required configurations
+$exportConfig = new ExportConfig();
+$exportConfig->set('chartConfig', realpath('resources/single.json'));
+// $exportConfig->set('quality', 'best');
+
+// Called on each export state change
+$onStateChange = function ($event) {
+    $state = $event->state;
+    echo('STATE: [' . $state->reporter . '] ' . $state->customMsg . "\n");
+};
+
+// Called when export is done
+$onDone = function ($event, $e) {
+    $export = $event->export;
+    if ($e) {
+        echo('ERROR: ' . $e->getMessage());
+    } else {
+        foreach ($export as $file) {
+            echo('DONE: ' . $file->realName. "\n");
+        }
+
+        ExportManager::saveExportedFiles($export);
+    }
+};
+
+// Instantiate the ExportManager class
+$exportManager = new ExportManager();
+// Call the export() method with the export config and the respective callbacks
+$exportManager->export($exportConfig, $onDone, $onStateChange);
