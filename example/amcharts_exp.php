e486b14e9e1a5f376bc9b4d0ab00c2c3c0bf2764
nibir <nibir@collabion.com>
2018-06-21 17:39:56 +0530

    - Added Samples for external libraries.

---
+++ example/amcharts_exp.php
@@ -0,0 +1,40 @@
+<?php
+
+// D3 export
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Use the sdk
+use FusionExport\ExportManager;
+use FusionExport\ExportConfig;
+
+// Instantiate the ExportConfig class and add the required configurations
+$exportConfig = new ExportConfig();
+$exportConfig->set('templateFilePath', realpath('resources/template_amcharts.html'));
+$exportConfig->set('type', 'jpg');
+$exportConfig->set('asyncCapture', 'true');
+
+// Called on each export state change
+$onStateChange = function ($event) {
+    $state = $event->state;
+    echo('STATE: [' . $state->reporter . '] ' . $state->customMsg . "\n");
+};
+
+// Called when export is done
+$onDone = function ($event, $e) {
+    $export = $event->export;
+    if ($e) {
+        echo('ERROR: ' . $e->getMessage());
+    } else {
+        foreach ($export as $file) {
+            echo('DONE: ' . $file->realName. "\n");
+        }
+
+        ExportManager::saveExportedFiles($export);
+    }
+};
+
+// Instantiate the ExportManager class
+$exportManager = new ExportManager();
+// Call the export() method with the export config and the respective callbacks
+$exportManager->export($exportConfig, $onDone, $onStateChange);
