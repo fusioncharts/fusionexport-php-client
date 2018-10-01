da7687ddd1ff6e0e802deb9bfa42d6d7c206ad1c
Jimut Dhali <jimutdhali@gmail.com>
2018-10-01 17:57:35 +0530

    Updated examples

--- example/chartjs_exp.php
+++ example/chartjs_exp.php
@@ -14,27 +14,7 @@ $exportConfig->set('templateFilePath', realpath('resources/template_chartjs.html
 $exportConfig->set('type', 'jpg');
 $exportConfig->set('asyncCapture', 'true');
 
-// Called on each export state change
-$onStateChange = function ($event) {
-    $state = $event->state;
-    echo('STATE: [' . $state->reporter . '] ' . $state->customMsg . "\n");
-};
-
-// Called when export is done
-$onDone = function ($event, $e) {
-    $export = $event->export;
-    if ($e) {
-        echo('ERROR: ' . $e->getMessage());
-    } else {
-        foreach ($export as $file) {
-            echo('DONE: ' . $file->realName. "\n");
-        }
-
-        ExportManager::saveExportedFiles($export);
-    }
-};
-
 // Instantiate the ExportManager class
 $exportManager = new ExportManager();
-// Call the export() method with the export config and the respective callbacks
-$exportManager->export($exportConfig, $onDone, $onStateChange);
+// Call the export() method with the export config
+$exportManager->export($exportConfig, '.', true);
--- example/single_export_ftp.php
+++ example/single_export_ftp.php
@@ -17,26 +17,7 @@ define("__FTP_LOGIN_PASSWORD", "");
 $exportConfig = new ExportConfig();
 $exportConfig->set('chartConfig', realpath('resources/single.json'));
 
-// Called on each export state change
-$onStateChange = function ($event) {
-    $state = $event->state;
-    echo('STATE: [' . $state->reporter . '] ' . $state->customMsg . "\n");
-};
-
-// Called when export is done
-$onDone = function ($event, $e) {
-    $export = $event->export;
-    if ($e) {
-        echo('ERROR: ' . $e->getMessage());
-    } else {
-        foreach ($export as $file) {
-            echo('DONE: ' . $file->realName. "\n");
-        }
-        ExportManager::uploadToFTP(__FTP_HOST, __FTP_PORT, __FTP_LOGIN_USERNAME, __FTP_LOGIN_PASSWORD, 'exported_images', $export);
-    }
-};
-
 // Instantiate the ExportManager class
 $exportManager = new ExportManager();
-// Call the export() method with the export config and the respective callbacks
-$exportManager->export($exportConfig, $onDone, $onStateChange);
+// Call the export() method with the export config
+$exportManager->export($exportConfig, '.', true);
