b238d4b7fd58600e7f3fa8c68b3671aba5f447b8
Dhiresh <dhiresh.kumar@appwrk.com>
2021-01-30 15:07:49 +0530

    IsSecure Test for dashboard

--- example/dashboard_export.php
+++ example/dashboard_export.php
@@ -11,10 +11,12 @@ use FusionExport\ExportConfig;
 $exportConfig = new ExportConfig();
 $exportConfig->set('chartConfig', realpath(__DIR__ . '/resources/multiple.json'));
 $exportConfig->set('templateFilePath', realpath(__DIR__ . '/resources/template.html'));
-$exportConfig->set('type', 'pdf');
-
+$exportConfig->set('type', 'png');
+$host = '127.0.0.1';
+$port = 1337;
+$isSecure = TRUE;
 // Instantiate the ExportManager class
-$exportManager = new ExportManager();
+$exportManager = new ExportManager($host, $port, $isSecure);
 // Call the export() method with the export config
 $files = $exportManager->export($exportConfig, '.', true);
 
